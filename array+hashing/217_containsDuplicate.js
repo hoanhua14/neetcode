f259da52f71c26e1616f3cecb80bde6d0c52052c
hoanhua14 <huasohoan@gmail.com>
2023-10-20 17:14:36 -0700

    contains dup js

---
+++ array+hashing/217_containsDuplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const checked = [];
+    for (let i of nums) {
+        if (checked.includes(i)) {
+            return true;
+        } else {
+            checked.push(i);
+        }
+    }
+    return false;
+};
