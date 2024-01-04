1286862ab2d4003460e6ceb3ea2206358046a9d2
hoanhua14 <huasohoan@gmail.com>
2024-01-03 16:03:22 -0800

    new solution checking dup

--- array+hashing/217_containsDuplicate.js
+++ array+hashing/217_containsDuplicate.js
@@ -3,14 +3,10 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const checked = [];
-    for (let i of nums) {
-        if (checked.includes(i)) {
-            return true;
-        } else {
-            checked.push(i);
-        }
+    let newSet = new Set(nums);
+    if (newSet.size == nums.length) {
+        return false;
+    } else {
+        return true;
     }
-    return false;
 };
- 
