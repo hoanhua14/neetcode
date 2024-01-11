28407aec4f997b44342d1b1437c84a26f02e911f
hoanhua14 <huasohoan@gmail.com>
2024-01-10 21:45:19 -0800

    2665 counter II

---
+++ leetcode/2665_counterII.js
@@ -0,0 +1,18 @@
+var createCounter = function(init) {
+    let count = init;
+    function increment() {
+        return ++count;
+    };
+    function decrement() {
+        return --count;
+    };
+    function reset() {
+        count = init;
+        return count;
+    };
+    return {
+        increment: increment,
+        decrement: decrement,
+        reset: reset
+    };
+};
