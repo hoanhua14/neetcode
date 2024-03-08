cbc5513e2af90179c7ec4c079d0109a7a1c17b78
hoanhua14 <huasohoan@gmail.com>
2024-03-08 15:53:08 -0800

    2726

---
+++ leetcode/2726_calculatorWithMethodChaining.js
@@ -0,0 +1,23 @@
+class Calculator {
+    constructor(value) {
+
+    }
+    add(value){
+
+    }
+    subtract(value){
+
+    }
+    multiply(value){
+
+    }
+    divide(value){
+
+    }
+    power(value){
+
+    }
+    getResult(value){
+
+    }
+}
