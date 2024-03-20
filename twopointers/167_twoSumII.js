daf0ee57752fbc99b74cbd84785c02f647eaa002
hoanhua14 <huasohoan@gmail.com>
2024-03-19 22:49:03 -0700

    optimized

--- twopointers/167_twoSumII.js
+++ twopointers/167_twoSumII.js
@@ -20,3 +20,24 @@ var twoSum = function(numbers, target) {
         }
     }
 };
+
+
+// input: an array of integer nums , a target integer num
+// output: indices of two numbers that add up to target
+// loop thru the array
+    // find the difference between that ele and target => give me a number d to look for
+    // check if index of d is valid
+        // if valid
+        // return an array of the 2 indices
+
+
+        var twoSum = function(nums, target) {
+            for (let i = 0; i < nums.length; i++) {
+                let diff = target - nums[i];
+                let indexOfDiff = nums.indexOf(diff)
+                if (indexOfDiff !== -1 && i !== indexOfDiff) {
+
+                    return [i, nums.indexOf(diff)]
+                }
+            }
+        };
