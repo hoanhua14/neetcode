f070050048cf20bde5d47c56c2aa988f4c617d0e
hoanhua14 <huasohoan@gmail.com>
2024-03-21 13:30:46 -0700

    find index of first occurence

---
+++ leetcode/28_findTheIndexOfTheFirstOccurenceInAString.js
@@ -0,0 +1,6 @@
+// input: 2 strings: needle and haystack
+// output: return the index of first occurence of needle in haystack. If not present, return -1
+
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
