269b47fa8646ea83881e5db4a2abe53f929c3962
hoanhua14 <huasohoan@gmail.com>
2024-03-21 16:39:40 -0700

    pseudo

---
+++ leetcode/14_longestCommonPrefix.js
@@ -0,0 +1,7 @@
+// input: given an array of strings
+// output: a string of longest common prefix, if not found return empty string
+
+// create a res empty string
+// loop thru the array of strings based on the length of first string
+    // iterate thru all strings make sure all strings have the same char at index i
+        //
---
+++ leetcode/27_removeElement.js
@@ -0,0 +1,2 @@
+// input: an integer array nums, an integer val. Remove all occurences of val in nums in place
+// output: return the number of ele in nums which are not equal to val
