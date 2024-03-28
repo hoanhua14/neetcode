8b8da8fa44178818322b3e3309d11bdd0df7f720
hoanhua14 <huasohoan@gmail.com>
2024-03-28 16:43:49 -0700

    27 js solution

--- leetcode/27_removeElement.js
+++ leetcode/27_removeElement.js
@@ -1,2 +1,12 @@
 // input: an integer array nums, an integer val. Remove all occurences of val in nums in place
 // output: return the number of ele in nums which are not equal to val
+var removeElement = function(nums, val) {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
