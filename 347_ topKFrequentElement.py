3d1f219e0df223b0bea5406add652ebe783ff7cf
hoanhua14 <huasohoan@gmail.com>
2024-03-27 21:22:29 -0700

    added all

--- 347_ topKFrequentElement.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-def topKFrequent(nums: list[int]):
-    numbers = sorted(nums)
-    hashSet = defaultdict(list)
-    print(hashSet)
-
-
-topKFrequent([1,1,1,2,2,3])
