e9c2e630a3343fa4e3097acf8c568d86691ed92b
hoanhua14 <huasohoan@gmail.com>
2024-04-12 15:51:51 -0700

    Boyer Moore approach

--- leetcode/169_majority_element.py
+++ leetcode/169_majority_element.py
@@ -22,3 +22,14 @@ class Solution:
 #             res = num if count[num] > maxCount else res
 #             maxCount = max(count[num], maxCount)
 #         return res
+
+
+# Boyer Moore
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         res, count = 0, 0
+#         for num in nums:
+#             if count == 0:
+#                 res = num
+#             count += (1 if num == res else -1)
+#         return res
