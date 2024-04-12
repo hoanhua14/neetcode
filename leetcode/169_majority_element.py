c8927d4c8c8974f6feae37e73a8192975ddc4892
hoanhua14 <huasohoan@gmail.com>
2024-04-12 15:36:24 -0700

    optimized 169

--- leetcode/169_majority_element.py
+++ leetcode/169_majority_element.py
@@ -13,3 +13,12 @@ class Solution:
                 maj = val
                 key_of_maj = key
         return key_of_maj
+
+# optimized:
+#         count = {}
+#         res, maxCount = 0, 0
+#         for num in nums:
+#             count[num] = 1 + count.get(num, 0)
+#             res = num if count[num] > maxCount else res
+#             maxCount = max(count[num], maxCount)
+#         return res
