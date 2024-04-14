df2dd42258688115515637bcc51dc9bcc9ad2f19
hoanhua14 <huasohoan@gmail.com>
2024-04-13 21:36:44 -0700

    different approach 392

--- leetcode/392_isSubseq.py
+++ leetcode/392_isSubseq.py
@@ -11,3 +11,17 @@ class Solution:
                 elif k < last_index:
                     k+=1
         return False
+
+
+# Different approach:
+        # i, j = 0, 0
+        # while (i < len(s) and j < len(t)):
+        #     if (s[i] == t[j]):
+        #         i+=1
+        #         j+=1
+        #     else:
+        #         j+=1
+        # if (i < len(s)):
+        #     return False
+        # else:
+        #     return True
