961153c2fee45575039f9551d9d6aa8b874fc78f
hoanhua14 <huasohoan@gmail.com>
2025-05-01 16:00:51 -0700

    optimized solution

--- array+hashing/217_containsDuplicate.js
+++ array+hashing/217_containsDuplicate.js
@@ -2,15 +2,14 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    let newSet = new Set(nums);
-    if (newSet.size == nums.length) {
-        return false;
-    } else {
-        return true;
-    }
+var containsDuplicate = function (nums) {
+  let newSet = new Set(nums);
+  if (newSet.size == nums.length) {
+    return false;
+  } else {
+    return true;
+  }
 };
-
 
 // input: an array of intergers nums
 // output: true if there's dup, false if distinct
@@ -22,32 +21,41 @@ var containsDuplicate = function(nums) {
 //  + else
 //      + return false
 
-const solution2 = function(nums) {
-    let checked = [];
-    for (let i = 0; i < nums.length; i++) {
-        if (!checked.has(nums[i])) {
-            checked.push(nums[i]);
-        } else {
-            return false;
-        }
+const solution2 = function (nums) {
+  let checked = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (!checked.has(nums[i])) {
+      checked.push(nums[i]);
+    } else {
+      return false;
     }
-    return true;
+  }
+  return true;
+};
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums) {
+    let checked = [];
+    for (let num of nums) {
+      if (checked.includes(num)) {
+        return true;
+        break;
+      } else {
+        checked.push(num);
+      }
+    }
+    return false;
+  }
 }
 class Solution {
-    /**
-     * @param {number[]} nums
-     * @return {boolean}
-     */
-    hasDuplicate(nums) {
-        let checked = []
-        for (let num of nums) {
-            if (checked.includes(num)){
-                return true;
-                break
-            } else {
-                checked.push(num)
-            }
-        }
-        return false
-    }
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums) {
+    return new Set(nums).size !== nums.length;
+  }
 }
