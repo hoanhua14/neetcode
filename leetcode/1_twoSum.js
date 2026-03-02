24d0b3ea3b16554e9bb750a9decdb0f742130b4b
hoanhua14 <huasohoan@gmail.com>
2026-03-02 12:53:40 -0800

    two sum js

---
+++ leetcode/1_twoSum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let i2 = i + 1; i2 < nums.length; i2++) {
+      if (nums[i] + nums[i2] == target) {
+        return [i, i2];
+      }
+    }
+  }
+};
