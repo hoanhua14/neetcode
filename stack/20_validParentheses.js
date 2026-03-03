983d25838ad88c8affa4461daed49af954397c37
hoanhua14 <huasohoan@gmail.com>
2026-03-03 13:55:17 -0800

    valid parentheses js

--- stack/20_validParentheses.js
+++ stack/20_validParentheses.js
@@ -1,22 +1,34 @@
 // input: string s
 // output: boolean, true if valid  and false if not
-var isValid = function(s) {
-    let pair = new Map([
-        [")", "("],
-        ["}", "{"],
-        ["]", "["]
-    ]);
-    let stack = []
-    for (let i of s) {
-        if (pair.has(i)) {
-            if (stack && stack[stack.length -1] == pair.get(i)) {
-                stack.pop();
-            } else {
-                return false;
-            }
-        } else {
-            stack.push(i);
-        }
+var isValid = function (s) {
+  let pair = new Map([
+    [")", "("],
+    ["}", "{"],
+    ["]", "["],
+  ]);
+  let stack = [];
+  for (let i of s) {
+    if (pair.has(i)) {
+      if (stack && stack[stack.length - 1] == pair.get(i)) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    } else {
+      stack.push(i);
     }
-    return stack.length === 0;
+  }
+  return stack.length === 0;
+};
+var isValid = function (s) {
+  let stack = [s[0]];
+  const mySet = { "(": ")", "[": "]", "{": "}" };
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] == mySet[stack[stack.length - 1]]) {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
+    }
+  }
+  return stack.length === 0;
 };
